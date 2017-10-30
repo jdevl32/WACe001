279c987b4b34cb9427d347896510b4d34dc81896
jdevl32 <2d75a844@opayq.com>
2017-10-30 11:37:23 -0400

    jdevl32 | [5] MVC 6 | [5.3] Enabling MVC 6

---
+++ WACe001/Controller/Web/AppController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WACe001.Controller.Web
+{
+
+	public class AppController : Microsoft.AspNetCore.Mvc.Controller
+	{
+
+		// GET: /<controller>/
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+	}
+
+}
--- WACe001/Startup.cs
+++ WACe001/Startup.cs
@@ -10,6 +10,7 @@ namespace WACe001
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddMvc();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,7 +26,9 @@ namespace WACe001
 			//	await context.Response.WriteAsync("What up!");
 			//});
 
-			app.UseDefaultFiles();
+			//app.UseDefaultFiles();
+
+			app.UseMvc(builder => builder.MapRoute(name: "Default", template: "{controller}/{action}/{id?}", defaults: new {controller = "App", action = "Index"}));
 			app.UseStaticFiles();
 		}
 	}
