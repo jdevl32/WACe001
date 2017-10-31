5efdce755815fbe43ab5502b49c351cd07887cd0
jdevl32 <2d75a844@opayq.com>
2017-10-31 00:12:18 -0400

    jdevl32 | [5] MVC 6 | [5.7] Implementing a Contact Page

---
+++ WACe001/Interface/IContact.cs
@@ -0,0 +1,15 @@
+﻿namespace WACe001.Interface
+{
+
+	public interface IContact
+	{
+
+		string Name { get; set; }
+
+		string Email { get; set; }
+
+		string Message { get; set; }
+
+	}
+
+}
---
+++ WACe001/ViewModel/ContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿using WACe001.Interface;
+
+namespace WACe001.ViewModel
+{
+
+	public class ContactViewModel
+		:
+		IContact
+	{
+
+		public string Name { get; set; }
+
+		public string Email { get; set; }
+
+		public string Message { get; set; }
+
+	}
+
+}
