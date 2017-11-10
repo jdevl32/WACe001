1205d219087a9bf3a97c8d9667ce60ade4efc22e
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-11-10 18:25:27 -0500

    jdevl32 | [10] Angular | [10.7] Retrieving Data with Angular

--- WACe001/wwwroot/script/controller/trip.js
+++ WACe001/wwwroot/script/controller/trip.js
@@ -13,79 +13,76 @@
 		{
 			var viewModel = this;
 
+			viewModel.isBusy = true;
 			viewModel.isDev = false;
 
 			// Create empty container for error message.
 			viewModel.errorMessage = "";
-			//viewModel.errorMessage = "[viewModel.errorMessage]";
 
 			// Create empty container for trip(s).
 			viewModel.trip = [];
 
-			viewModel.isBusy = true;
+			// Create empty container for new trip.
+			viewModel.newTrip = {};
 
-			// /**
+			// Create success handler.
+			var onSuccess =
+				function(response)
+				{
+					// todo|jdevl32: make this global method...
+					// todo|jdevl32: fix (is-dev not working) !!!
+					if (viewModel.isDev)
+					{
+						var x = "[response=";
+
+						for (var p in response)
+						{
+							if (!response.hasOwnProperty(p))
+							{
+								continue;
+							} // if
+
+							if ("" !== x)
+							{
+								x += "\n";
+							} // if
+
+							x += "[response." + p + "=" + response[p] + "]";
+						} // for
+
+						x += "]";
+
+						alert(x);
+					} // if
+
+					angular.copy(response.data, viewModel.trip);
+				};
+
+			// Create error handler.
+			var onError =
+				function(error)
+				{
+					viewModel.errorMessage = "Failed to get trips:  " + error;
+				};
+
+			// Create finally handler.
+			var doFinally =
+				function()
+				{
+					// Reset busy flag.
+					viewModel.isBusy = false;
+				};
+
 			try
 			{
-				$http.get("/api/trip")
-					.then
-						(
-							// Success:
-							function (response)
-							{
-								// todo|jdevl32: make this global method...
-								// todo|jdevl32: fix (is-dev not working) !!!
-								if(viewModel.isDev)
-								{
-									var x = "[response=";
-
-									for (var p in response)
-									{
-										if (!response.hasOwnProperty(p))
-										{
-											continue;
-										} // if
-
-										if ("" !== x)
-										{
-											x += "\n";
-										} // if
-
-										x += "[response." + p + "=" + response[p] + "]";
-									} // for
-
-									x += "]";
-
-									alert(x);
-								} // if
-
-								angular.copy(response.data, viewModel.trip);
-							}
-							,
-							// Failure:
-							function (error)
-							{
-								viewModel.errorMessage = "Failed to get trips:  " + error;
-							}
-						)
-					.finally
-						(
-							function ()
-							{
-								viewModel.isBusy = false;
-							}
-						);
-
+				// Get the set of trips from the API, using the defined handlers.
+				$http.get("/api/trip").then(onSuccess, onError).finally(doFinally);
 			} // try
 			catch (e)
 			{
 				viewModel.isBusy = false;
 				viewModel.errorMessage = "Failed to get trips:  " + e;
 			} // catch
-			//**/
-
-			// Create empty container for new trip.
-			viewModel.newTrip = {};
 
 			// Form submit handler.
 			viewModel.AddTrip =
