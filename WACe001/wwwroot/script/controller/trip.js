468bb1e1e9727843ab5b550c877ba1334dbb727a
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-11-10 15:17:00 -0500

    jdevl32 | [10] Angular | [10.7] Retrieving Data with Angular

--- WACe001/wwwroot/script/controller/trip.js
+++ WACe001/wwwroot/script/controller/trip.js
@@ -7,12 +7,82 @@
 		"use strict";
 
 		// Define the trip controller.
-		function trip()
+		// Last modification:
+		// Add the http service dependency.
+		function trip($http)
 		{
 			var viewModel = this;
 
+			viewModel.isDev = false;
+
+			// Create empty container for error message.
+			viewModel.errorMessage = "";
+			//viewModel.errorMessage = "[viewModel.errorMessage]";
+
 			// Create empty container for trip(s).
 			viewModel.trip = [];
+
+			viewModel.isBusy = true;
+
+			// /**
+			try
+			{
+				$http.get("/api/trip")
+					.then
+						(
+							// Success:
+							function (response)
+							{
+								// todo|jdevl32: make this global method...
+								// todo|jdevl32: fix (is-dev not working) !!!
+								if(viewModel.isDev)
+								{
+									var x = "[response=";
+
+									for (var p in response)
+									{
+										if (!response.hasOwnProperty(p))
+										{
+											continue;
+										} // if
+
+										if ("" !== x)
+										{
+											x += "\n";
+										} // if
+
+										x += "[response." + p + "=" + response[p] + "]";
+									} // for
+
+									x += "]";
+
+									alert(x);
+								} // if
+
+								angular.copy(response.data, viewModel.trip);
+							}
+							,
+							// Failure:
+							function (error)
+							{
+								viewModel.errorMessage = "Failed to get trips:  " + error;
+							}
+						)
+					.finally
+						(
+							function ()
+							{
+								viewModel.isBusy = false;
+							}
+						);
+
+			} // try
+			catch (e)
+			{
+				viewModel.isBusy = false;
+				viewModel.errorMessage = "Failed to get trips:  " + e;
+			} // catch
+			//**/
 
 			// Create empty container for new trip.
 			viewModel.newTrip = {};
